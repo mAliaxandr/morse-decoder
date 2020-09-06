45ea3230a093e799560da2e0fad3ada447758f81
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2020-09-06 23:32:19 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,51 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const input = expr;
+    console.log('------', input);
+    const arrInput  = input.split('');
+    console.log('res -- ', arrInput);
+    let letter = '';
+    let text = '';
+    arrInput.forEach( item => {
+        letter = letter.concat(item);
+        console.log('-l- ', letter, item);
+        if (letter == '**********'){
+            text = text.concat(' ');
+            letter = '';
+        }
+        if (letter.length == 10) {
+            console.log(' -letter- ', letter);
+            
+            const key = getKey(letter);
+            getLetter(key);
+            letter = '';
+        }
+    })
+
+    function getKey(str) {
+        let key = '';
+        for (var i = 0; i < str.length; i = i+2) {
+            let simbol = str[i].concat(str[i+1])
+            if ( simbol == '10') {
+                key = key.concat('.');
+            }
+            if ( simbol == '11') {
+                key = key.concat('-');
+            }
+            console.log('simbol -- ', simbol);
+        }
+        console.log('key -- ', key);
+        return key;
+    }
+
+    function getLetter(key) {
+        text = text.concat(MORSE_TABLE[key]);
+        console.log('-----', key, MORSE_TABLE[key], text);
+    }
+    console.log('result --------', text);
+
+    return text;
 }
 
 module.exports = {
